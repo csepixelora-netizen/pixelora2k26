faa7c96736c6e1d76dbac1044459393c7df5ebe4
tyrant1669 <pranavkumaran2005@gmail.com>
2026-04-03 20:51:50 +0530

    Update Firebase config for PIXELORA 2K26

--- frontend/config.js
+++ frontend/config.js
@@ -1,11 +1,12 @@
 window.__PIXELORA_CONFIG__ = {
   apiBaseUrl: "https://pixelora-9git.onrender.com",
   firebase: {
-    apiKey: "AIzaSyCIQpN6-kbbbLYrbuWX33ulmcqa12Gld5w",
-    authDomain: "pixelora-30fdb.firebaseapp.com",
-    projectId: "pixelora-30fdb",
-    storageBucket: "pixelora-30fdb.firebasestorage.app",
-    messagingSenderId: "652438003040",
-    appId: "1:652438003040:web:7160736d896162e39b5205"
+    apiKey: "AIzaSyBkuiamDyaBeu4tmtKv9Y54xrlmFDklCV0",
+    authDomain: "pixelora2k26.firebaseapp.com",
+    projectId: "pixelora2k26",
+    storageBucket: "pixelora2k26.firebasestorage.app",
+    messagingSenderId: "828857506457",
+    appId: "1:828857506457:web:ee4c93ac53dff8ad2b3608",
+    measurementId: "G-94ZKZCFLY2"
   }
 };
